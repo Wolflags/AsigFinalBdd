3ef114b36eb3c875ab86c49451bc9edf5511ce34
Owner <Owner@DESKTOP-JC2PUTJ>
2023-07-25 20:17:02 -0400

    un reguero

--- Bdd/src/visual/Inicio.java
+++ Bdd/src/visual/Inicio.java
@@ -8,10 +8,15 @@ import java.awt.Toolkit;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.border.TitledBorder;
+import java.awt.Color;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import javax.swing.JLabel;
 
 public class Inicio extends JFrame {
 
@@ -37,75 +42,127 @@ public class Inicio extends JFrame {
 	 * Create the frame.
 	 */
 	public Inicio() {
-		setTitle("Academico");
+		setBackground(new Color(255, 255, 255));
+		setTitle("Inicio");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		 setSize(1377, 725);
+		 setSize(1043, 725);
 		 Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		    int x = (screenSize.width - getWidth()) / 2;
 		    int y = (screenSize.height - getHeight()-30) / 2;
 		    setLocation(x, y);
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(255, 255, 255));
+		contentPane.setForeground(new Color(64, 0, 64));
 		contentPane.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JButton btnNewButton = new JButton("Grupos");
+		JButton btnNewButton = new JButton("");
+		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 15));
+		btnNewButton.setSelectedIcon(null);
+		btnNewButton.setBackground(Color.LIGHT_GRAY);
+		btnNewButton.setForeground(Color.WHITE);
+		btnNewButton.setIcon(new ImageIcon(Inicio.class.getResource("/Images/examen2.png")));
+		
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				 // Crea una nueva instancia de tu JDialog
 		        ListarGrupos dialog = new ListarGrupos();
 		        dialog.setVisible(true); // Muestra el JDialog
 		        // Cierra el JFrame actual
-		        dispose();
+		       
 			}
 		});
-		btnNewButton.setBounds(83, 86, 185, 53);
+		btnNewButton.setBounds(432, 354, 173, 175);
 		contentPane.add(btnNewButton);
 		
-		JButton btnNewButton_1 = new JButton("Estudiantes");
-		btnNewButton_1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				//Crea una nueva instancia de tu JDialog
-				Estudiante dialog = new Estudiante();
-				dialog.setVisible(true);//Muestra el JDialog
-				//Cierra el JFrame actual
-				dispose();
-			}
-		});
-		
-		btnNewButton_1.setBounds(318, 86, 185, 50);
-		contentPane.add(btnNewButton_1);
-		
-	
-		
-		btnNewButton_1.setBounds(318, 88, 185, 48);
-		contentPane.add(btnNewButton_1);
-		
-		JButton btnNewButton_2 = new JButton("Periodo Academico");
+		JButton btnNewButton_2 = new JButton("");
+		btnNewButton_2.setIcon(new ImageIcon(Inicio.class.getResource("/Images/calendario3.png")));
+		btnNewButton_2.setFont(new Font("Tahoma", Font.BOLD, 15));
 		btnNewButton_2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				//Crea una nueva instancia de tu JDialog
 				PeriodoAcademico dialog = new PeriodoAcademico();
 				dialog.setVisible(true);//Muestra el JDialog
 				//Cierra el JFrame actual
-				dispose();
+				
 			}
 		});
-		btnNewButton_2.setBounds(545, 88, 164, 51);
+		btnNewButton_2.setBounds(772, 85, 173, 175);
 		contentPane.add(btnNewButton_2);
 		
-		JButton btnNewButton_3 = new JButton("Asignaturas");
+		JButton btnNewButton_3 = new JButton("");
+		btnNewButton_3.setIcon(new ImageIcon(Inicio.class.getResource("/Images/Boton.png")));
+		btnNewButton_3.setFont(new Font("Tahoma", Font.BOLD, 15));
 		btnNewButton_3.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				Asignatura dialog = new Asignatura();
 				dialog.setVisible(true);//Muestra el JDialog
 				//Cierra el JFrame actual
-				dispose();
+				
 			}
 		});
-		btnNewButton_3.setBounds(83, 208, 185, 53);
+		btnNewButton_3.setBounds(96, 85, 173, 175);
 		contentPane.add(btnNewButton_3);
+		
+		JLabel lblNewLabel = new JLabel("Grupos");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblNewLabel.setBounds(437, 407, 79, 14);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Asignaturas");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblNewLabel_1.setBounds(128, 271, 111, 14);
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("Periodo Académico");
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblNewLabel_2.setBounds(782, 271, 164, 14);
+		contentPane.add(lblNewLabel_2);
+		
+		JButton btnNewButton_1 = new JButton("");
+		btnNewButton_1.setIcon(new ImageIcon(Inicio.class.getResource("/Images/estudiante4.png")));
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//Crea una nueva instancia de tu JDialog
+				Estudiante dialog = new Estudiante();
+				dialog.setVisible(true);//Muestra el JDialog
+				//Cierra el JFrame actual
+				
+			}
+		});
+		btnNewButton_1.setBounds(96, 354, 173, 175);
+		contentPane.add(btnNewButton_1);
+		
+		JLabel lblNewLabel_3 = new JLabel("Estudiantes");
+		lblNewLabel_3.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblNewLabel_3.setBounds(130, 545, 109, 14);
+		contentPane.add(lblNewLabel_3);
+		
+		JButton btnNewButton_4 = new JButton("");
+		btnNewButton_4.setIcon(new ImageIcon(Inicio.class.getResource("/Images/inscripcion1.png")));
+		btnNewButton_4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//Crea una nueva instancia de tu JDialog
+				Incripcion dialog = new Incripcion();
+				dialog.setVisible(true);//Muestra el JDialog
+				//Cierra el JFrame actual
+				
+			}
+		});
+		btnNewButton_4.setBounds(421, 85, 173, 175);
+		contentPane.add(btnNewButton_4);
+		
+		JLabel lblNewLabel_4 = new JLabel("Inscripción");
+		lblNewLabel_4.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblNewLabel_4.setBounds(453, 272, 103, 14);
+		contentPane.add(lblNewLabel_4);
+		
+		JLabel lblNewLabel_3_1 = new JLabel("Grupos");
+		lblNewLabel_3_1.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblNewLabel_3_1.setBounds(485, 545, 109, 14);
+		contentPane.add(lblNewLabel_3_1);
 		
 	}
 }
